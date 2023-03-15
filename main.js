8eff8911ba54980a477d6846a990f9c63ce7d2f9
Ravi1057123 <p.ravi@voltuswave.com>
2023-03-15 22:46:01 +0530

    Add:List Rendering For Vue js

--- main.js
+++ main.js
@@ -4,7 +4,18 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                {id:100,color:'red'},
+                {id:101,color:'yellow'}
+            ],
+            sizes:[
+               
+                    { id: 1, size: 'Small' },
+                    { id: 2, size: 'Medium' },
+                    { id: 3, size: 'Large' },
+                    { id: 4, size: 'XL' }     
+            ]
         }
     }
 })
